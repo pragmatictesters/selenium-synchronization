fa0c19de0bddcca61c84349e634794bf004911e2
Janesh Kodikara <janesh@pragmatictesters.com>
2025-03-19 09:17:18 +0530

    Optimize the imports

--- src/test/java/com/pragmatic/synchronisation/wait/FluentWaitExampleTest.java
+++ src/test/java/com/pragmatic/synchronisation/wait/FluentWaitExampleTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
