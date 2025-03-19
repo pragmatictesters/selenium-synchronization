4aa394d4bba95cfdac2a6305cd8808045e668be6
Janesh Kodikara <janesh@pragmatictesters.com>
2025-03-19 09:24:39 +0530

    Refactor code with improved comments, structured formatting, and optimized imports

--- src/test/java/com/pragmatic/synchronisation/wait/VerifyThrownExceptionsTest.java
+++ src/test/java/com/pragmatic/synchronisation/wait/VerifyThrownExceptionsTest.java
@@ -10,49 +10,52 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class VerifyThrownExceptionsTest {
 
     WebDriver driver;
 
     @BeforeMethod
     public void setup() {
-        // Initialize WebDriver (use ChromeDriver as an example)
+        // Initialize WebDriver (using ChromeDriver as an example)
         driver = new ChromeDriver();
 
-        // Navigate to login page
+        // Navigate to the login page
         driver.get("https://pragmatictesters.github.io/selenium-synchronization/buttons.html");
 
-        // Maximize browser window
+        // Maximize the browser window
         driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        // Close the browser
+        // Close the browser if the driver is not null
         if (driver != null) {
             driver.quit();
         }
     }
 
-
+    /**
+     * Test to verify the NoSuchElementException is thrown when an element is not found.
+     */
     @Test
     public void testNoSuchElementException() {
+        // Click the "Start" button
         driver.findElement(By.id("easy00")).click();
-        Assert.assertThrows(NoSuchElementException.class,
-                () -> {
-                    driver.findElement(By.id("easy011")).click();
-                });
+
+        // Assert that a NoSuchElementException is thrown when attempting to find a non-existent element
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+            driver.findElement(By.id("easy011")).click();
+        });
     }
 
+    /**
+     * Test to verify the ElementNotInteractableException is thrown when an element is not interactable.
+     */
     @Test
     public void testElementNotInteractableException() {
-        Assert.assertThrows(ElementNotInteractableException.class,
-                () -> {
-                    driver.findElement(By.id("easy01")).click();
-                });
+        // Assert that an ElementNotInteractableException is thrown when trying to interact with a hidden element
+        Assert.assertThrows(ElementNotInteractableException.class, () -> {
+            driver.findElement(By.id("easy01")).click();
+        });
     }
-
-
 }
